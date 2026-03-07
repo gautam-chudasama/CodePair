05160a6f2a45edf8a9cdce6b987706a9a6b6772f
gautam-chudasama <gautam.chudasama@outlook.com>
2026-03-07 21:29:01 +0530

    first commit in backend branch

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,27 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
+import path from "path";
 
 const app = express();
+
+// const __dirname = path.resolve();
 
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-    res.json({message:"Hello World!"})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+//   app.get("/{*any}", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
