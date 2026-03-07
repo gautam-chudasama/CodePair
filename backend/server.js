2fc5c0cc9c5c6598a808b4a43443f9f7c58195e7
gautam-chudasama <gautam.chudasama@outlook.com>
2026-03-07 20:59:25 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
+
 const PORT = process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    res.json({message:"Hello World!"})
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
