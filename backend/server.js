6e6e9e39e975d14f59302d2c78822775cb777771
gautam-chudasama <gautam.chudasama@outlook.com>
2026-03-07 22:58:07 +0530

    Db setup, clerk auth, inggest, stream implemented

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,28 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import connectDb from "./src/lib/db.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 
 // const __dirname = path.resolve();
 
 app.use(cors());
+app.use(clerkMiddleware());
 
 const PORT = process.env.PORT || 3000;
+
+// // Use requireAuth() to protect this route
+// // If user isn't authenticated, requireAuth() will redirect back to the homepage
+// app.get('/protected', requireAuth(), async (req, res) => {
+//   // Use `getAuth()` to get the user's `userId`
+//   const { userId } = getAuth(req)
+
+//   // Use Clerk's JavaScript Backend SDK to get the user's User object
+//   const user = await clerkClient.users.getUser(userId)
+
+//   return res.json({ user })
+// })
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
